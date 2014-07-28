fcd2c5adc437163e080c36b582e4796c9265a620
ssledz <slawomir.sledz@sof-tech.pl>
2014-07-28 23:57:05 +0200

    bzz

--- stackoverflow-java-answers/src/main/java/pl/softech/stackoverflow/javac/DynamicProxy.java
+++ stackoverflow-java-answers/src/main/java/pl/softech/stackoverflow/javac/DynamicProxy.java
@@ -12,6 +12,7 @@ import java.net.URI;
 import java.security.SecureClassLoader;
 import java.util.Arrays;
 
+import javax.tools.Diagnostic;
 import javax.tools.DiagnosticCollector;
 import javax.tools.FileObject;
 import javax.tools.ForwardingJavaFileManager;
@@ -23,142 +24,163 @@ import javax.tools.SimpleJavaFileObject;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 
-//http://stackoverflow.com/questions/24994802/java-transform-string-into-code
+/**
+ * JAVA transform string into code
+ * http://stackoverflow.com/questions/24994802/java-transform-string-into-code 
+ */
 public class DynamicProxy {
 
-    public interface CalculateScore {
-	double calculate(double score);
-    }
-
-    private static class JavaSourceFromString extends SimpleJavaFileObject {
-	final String code;
-
-	JavaSourceFromString(String name, String code) {
-	    super(URI.create("string:///" + name.replace('.', '/') + Kind.SOURCE.extension), Kind.SOURCE);
-	    this.code = code;
+	public interface CalculateScore {
+		double calculate(double score);
 	}
 
-	@Override
-	public CharSequence getCharContent(boolean ignoreEncodingErrors) {
-	    return code;
-	}
-    }
+	private static class JavaSourceFromString extends SimpleJavaFileObject {
+		final String code;
 
-    private static class JavaClassObject extends SimpleJavaFileObject {
-
-	protected final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-	public JavaClassObject(String name, Kind kind) {
-	    super(URI.create("string:///" + name.replace('.', '/') + kind.extension), kind);
-	}
-
-	public byte[] getBytes() {
-	    return bos.toByteArray();
-	}
-
-	@Override
-	public OutputStream openOutputStream() throws IOException {
-	    return bos;
-	}
-    }
-
-    private static class ClassFileManager extends ForwardingJavaFileManager<StandardJavaFileManager> {
-
-	private JavaClassObject classObject;
-	private final String className;
-
-	public ClassFileManager(StandardJavaFileManager standardManager, String className) {
-	    super(standardManager);
-	    this.className = className;
-	}
-
-	@Override
-	public ClassLoader getClassLoader(Location location) {
-	    return new SecureClassLoader(DynamicProxy.class.getClassLoader()) {
-		@Override
-		protected Class<?> findClass(String name) throws ClassNotFoundException {
-
-		    if (name.contains(className)) {
-
-			byte[] b = classObject.getBytes();
-			return super.defineClass(name, classObject.getBytes(), 0, b.length);
-		    }
-
-		    return super.findClass(name);
+		JavaSourceFromString(String name, String code) {
+			super(URI.create("string:///" + name.replace('.', '/') + Kind.SOURCE.extension), Kind.SOURCE);
+			this.code = code;
 		}
-	    };
+
+		@Override
+		public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+			return code;
+		}
 	}
 
-	@Override
-	public JavaFileObject getJavaFileForOutput(Location location, String className,
-		javax.tools.JavaFileObject.Kind kind, FileObject sibling) throws IOException {
-	    classObject = new JavaClassObject(className, kind);
-	    return classObject;
+	private static class JavaClassObject extends SimpleJavaFileObject {
+
+		protected final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+		public JavaClassObject(String name, Kind kind) {
+			super(URI.create("string:///" + name.replace('.', '/') + kind.extension), kind);
+		}
+
+		public byte[] getBytes() {
+			return bos.toByteArray();
+		}
+
+		@Override
+		public OutputStream openOutputStream() throws IOException {
+			return bos;
+		}
 	}
 
-    }
+	private static class ClassFileManager extends ForwardingJavaFileManager<StandardJavaFileManager> {
 
-    private static class MyInvocationHandler implements InvocationHandler {
+		private JavaClassObject classObject;
+		private final String className;
 
-	private final String className;
-	private final String classBody;
+		public ClassFileManager(StandardJavaFileManager standardManager, String className) {
+			super(standardManager);
+			this.className = className;
+		}
 
-	public MyInvocationHandler(String implClassName, String classBody) {
-	    this.className = implClassName;
-	    this.classBody = classBody;
+		@Override
+		public ClassLoader getClassLoader(Location location) {
+			return new SecureClassLoader(DynamicProxy.class.getClassLoader()) {
+				@Override
+				protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+					if (name.contains(className)) {
+
+						byte[] b = classObject.getBytes();
+						return super.defineClass(name, classObject.getBytes(), 0, b.length);
+					}
+
+					return super.findClass(name);
+				}
+			};
+		}
+
+		@Override
+		public JavaFileObject getJavaFileForOutput(Location location, String className, javax.tools.JavaFileObject.Kind kind, FileObject sibling) throws IOException {
+			classObject = new JavaClassObject(className, kind);
+			return classObject;
+		}
+
 	}
 
-	@Override
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+	private static class MyInvocationHandler implements InvocationHandler {
 
-	    Class<?> clazz = compileClass(className, classBody);
+		private final String className;
+		private final String classBody;
 
-	    return method.invoke(clazz.newInstance(), args);
+		public MyInvocationHandler(String implClassName, String classBody) {
+			this.className = implClassName;
+			this.classBody = classBody;
+		}
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+			Class<?> clazz = compileClass(className, classBody);
+
+			return method.invoke(clazz.newInstance(), args);
+		}
+
 	}
 
-    }
+	private static Class<?> compileClass(String className, String classBody) throws Throwable {
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+		JavaFileObject file = new JavaSourceFromString(className, classBody);
+		Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(file);
 
-    private static Class<?> compileClass(String className, String classBody) throws Throwable {
-	JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-	DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
-	JavaFileObject file = new JavaSourceFromString(className, classBody);
-	Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(file);
+		JavaFileManager fileManager = new ClassFileManager(compiler.getStandardFileManager(null, null, null), className);
+		
+		CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
 
-	CompilationTask task = compiler.getTask(null, null, diagnostics, null, null, compilationUnits);
+		
 
-	JavaFileManager fileManager = new ClassFileManager(compiler.getStandardFileManager(null, null, null), className);
+		boolean success = task.call();
 
-	boolean success = task.call();
+		for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
+		      System.out.println(diagnostic.getCode());
+		      System.out.println(diagnostic.getKind());
+		      System.out.println(diagnostic.getPosition());
+		      System.out.println(diagnostic.getStartPosition());
+		      System.out.println(diagnostic.getEndPosition());
+		      System.out.println(diagnostic.getSource());
+		      System.out.println(diagnostic.getMessage(null));
 
-	if (success) {
-	    return fileManager.getClassLoader(null).loadClass(className);
+		    }
+		
+		if (success) {
+			return fileManager.getClassLoader(null).loadClass(className);
+		}
+
+		return null;
+
 	}
 
-	return null;
+	@SuppressWarnings("unchecked")
+	public static <T> T newProxyInstance(Class<T> clazz, String className, String classBody) {
 
-    }
+		ClassLoader parentLoader = DynamicProxy.class.getClassLoader();
 
-    @SuppressWarnings("unchecked")
-    public static <T> T newProxyInstance(Class<T> clazz, String className, String classBody) {
+		return (T) Proxy.newProxyInstance(parentLoader, new Class[] { clazz }, new MyInvocationHandler(className, classBody));
 
-	ClassLoader parentLoader = DynamicProxy.class.getClassLoader();
+	}
+	
+	public static CalculateScore create(String body) {
+		
+		StringWriter writer = new StringWriter();
+		PrintWriter out = new PrintWriter(writer);
+		out.println("public class CalculateScoreImpl implements  pl.softech.stackoverflow.javac.DynamicProxy.CalculateScore {");
+		out.println("  public double calculate(double score) {");
+		out.println(body);
+		out.println("  }");
+		out.println("}");
+		out.close();
+		
+		return newProxyInstance(CalculateScore.class, "CalculateScoreImpl", writer.toString());
+		
+	}
 
-	return (T) Proxy.newProxyInstance(parentLoader, new Class[] { clazz }, new MyInvocationHandler(className,
-		classBody));
-
-    }
-
-    public static void main(String[] args) {
-	StringWriter writer = new StringWriter();
-	PrintWriter out = new PrintWriter(writer);
-	out.println("public class CalculateScoreImpl implements CalculateScore {");
-	out.println("  public double calculate(double score) {");
-	out.println("    if (score <= 0.7) {return 0;} else if (score <=0.8) {return 1;} else if (score <=0.9) {return 2;} else {return 3}\");");
-	out.println("  }");
-	out.println("}");
-	out.close();
-	CalculateScore c = newProxyInstance(CalculateScore.class, "CalculateScoreImpl", writer.toString());
-	System.out.println(c.calculate(123));
-    }
+	public static void main(String[] args) {
+		CalculateScore calculator = create("if (score <= 0.7) {return 0;} else if (score <=0.8) {return 1;} else if (score <=0.9) {return 2;} else {return 3; }");
+		System.out.println(calculator.calculate(0.3));
+	}
 
 }
