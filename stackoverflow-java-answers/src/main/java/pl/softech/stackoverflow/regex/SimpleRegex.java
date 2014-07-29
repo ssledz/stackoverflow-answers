0654d35c2b665dcc498826c0bf744c813d012040
Slawomir Sledz <slawomir.sledz@contractors.roche.com>
2014-07-29 09:41:21 +0200

    bzz

---
+++ stackoverflow-java-answers/src/main/java/pl/softech/stackoverflow/regex/SimpleRegex.java
@@ -0,0 +1,25 @@
+package pl.softech.stackoverflow.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SimpleRegex {
+
+    static void example1() {
+	String s = "@123456@ 123456789[ 123456@ 12";
+	Pattern p = Pattern.compile("@(\\d{6})@\\s?(\\d{9}\\[\\s?\\d{6})@\\s?(\\d{2})");
+	Matcher m = p.matcher(s);
+	if (m.matches()) {
+	    System.out.println(m.group(1));
+	    System.out.println(m.group(2));
+	    System.out.println(m.group(3));
+	}
+    }
+
+    public static void main(String[] args) {
+
+	example1();
+
+    }
+
+}
